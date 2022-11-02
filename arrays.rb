1168ba84f6e0a3d01a93d5dfa09613887513c4d1
David Marino <davidjmarino8@gmail.com>
2022-11-02 16:14:13 -0400

    Add methods and comment

--- arrays.rb
+++ arrays.rb
@@ -2,3 +2,13 @@ friends = ["meeps", "sweeps", "bleeps", "heeps"]
 foes = [0, 1, 2, 3]
 money = [100.00, 101.00, 102.00, 103.00]
 crazy_people = [false, true, false, true]
+
+# Pop removes the last element of friends "heeps".
+# Push allowes me to enter in the element 4 at tghe end of the array.
+# Shift removes the first element of money, 100.00.
+# Unshift allows you to add an element to the beginning (true).
+
+friends.pop
+foes.push(4)
+money.shift
+crazy_people.unshift(true)
